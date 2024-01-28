b494332094077e5b84e83f8e9d428817f95e5e4f
JAY\xenon <dlaworud@koreatech.ac.kr>
2024-01-29 01:40:09 +0900

    자바 스터디 - generic

--- src/edu/ssafy/im/Hello.java
+++ src/edu/ssafy/im/Hello.java
@@ -1,5 +1,55 @@
 package edu.ssafy.im;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Hello {
+    class Iron extends Human{
+        int x;
 
+        Iron(int x) {
+            super(x);
+            this.x = x;
+        }
+
+        @Override
+        public int compareTo(Human o) {
+            // TODO Auto-generated method stub
+            return this.x - o.x;
+        }
+    }
+
+    class Human implements Comparable<Human> {
+        int x;
+
+        Human(int x) {
+            this.x = x;
+        }
+
+        @Override
+        public int compareTo(Human o) {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+    private void a() {
+        List<Human> arr = new ArrayList<>();
+        Iron i = new Iron(2);
+        Human h = new Human(1);
+        arr.add(i);
+        arr.add(h);
+        for (Human h1:
+                sorted(arr)) {
+            System.out.println(h1.x);
+        }
+    }
+
+    public <T extends Comparable<? super T>> List<T> sorted(List<T> list) {
+        return list;
+    }
 }
