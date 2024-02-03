48c64b59763a525192c78993684f95369efc2487
JAY\xenon <dlaworud@koreatech.ac.kr>
2024-02-03 12:05:08 +0900

    BOJ 2493. 탑

--- src/edu/ssafy/im/BOJ/Gold/G5/No2493/Main.java
+++ src/edu/ssafy/im/BOJ/Gold/G5/No2493/Main.java
@@ -3,12 +3,10 @@ package edu.ssafy.im.BOJ.Gold.G5.No2493;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-	StringBuilder sb = new StringBuilder();
-	int[] arr, ans;
-	int n;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		new Main().sol();
@@ -16,43 +14,45 @@ public class Main {
 
 	private void sol() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Stack<Tower> stack = new Stack<>();
+		for (int i = 1; i <= n; i++) {
+			int h = Integer.parseInt(st.nextToken());
+			if(stack.isEmpty()) {
+				sb.append("0 ");
+				stack.push(new Tower(i, h));
+			} else {
+				while (true) {
+					if (stack.isEmpty()) {
+						sb.append("0 ");
+						stack.push(new Tower(i, h));
+						break;
+					}
 
-		n = Integer.parseInt(br.readLine());
-		String input = br.readLine();
-		StringTokenizer st = new StringTokenizer(input);
-		arr = new int[n];
-		ans = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+					Tower t = stack.peek();
 
-		recursive(0, 0);
-		
-		for (int i = 0; i < ans.length; i++) {
-			System.out.print(ans[i] + " ");
+					if(t.height > h) {
+						sb.append(t.index + " ");
+						stack.push(new Tower(i, h));
+						break;
+					} else {
+						stack.pop();
+					}
+				}
+			}
 		}
+		System.out.print(sb);
 	}
 
-	private void recursive(int i, int j) {
-		// basis part
-		if (i == n)
-			return;
+	class Tower {
+		int index;
+		int height;
 
-		if (j < 0) {
-			ans[i] = 0;
-			return;
+		public Tower(int index, int height) {
+			this.index = index;
+			this.height = height;
 		}
-
-		if (arr[i] < arr[j]) {
-			ans[i] = j+1;
-			recursive(i + 1, i);
-			return;
-		}
-		// inductive part
-		recursive(i, j - 1);
-		
-		if(ans[i] == 0)
-			recursive(i + 1, i);
 	}
-	// 탑
 }
