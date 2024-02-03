228532ca5651caf1fd7bcf2251627a71fdb7c0e7
JAY\xenon <dlaworud@koreatech.ac.kr>
2024-02-03 17:24:32 +0900

    Revert "BOJ 6987. 탑"
    This reverts commit 969100d8fd76dcb6f2ec4417e35fdd574a45eb18.

--- src/edu/ssafy/im/BOJ/Gold/G4/No6987/Main.java
+++ src/edu/ssafy/im/BOJ/Gold/G4/No6987/Main.java
@@ -7,125 +7,80 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    int arr[][], match[][];
-    int ans;
-    boolean flag;
+	int ans;
+	int[][] arr;
+	boolean[] visited;
+	int n = 5;
+	StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		new Main().sol();
+	}
 
-    public static void main(String[] args) throws IOException {
-        new Main().sol();
-    }
+	private void sol() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 
-    private void sol() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        // 입력
-        for (int t = 0; t < 4; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr = new int[6][3];
-            ans = 1;
-            boolean isPossible = true;
-            L:
-            for (int i = 0; i < 6; i++) {
-                int sum = 0;
-                for (int j = 0; j < 3; j++) {
-                    // i : 팀 j : 승,무,패
-                    arr[i][j] = Integer.parseInt(st.nextToken());
-                    sum += arr[i][j];
-                    // 1. 승 무 패 가 5보다 큰 경우
-                    if (arr[i][j] > 5) {
-                        isPossible = false;
-                        break L;
-                    }
-                }
-                // 2. 승 무 패 의 합이 5 가 아닌 경우
-                if (sum != 5) {
-                    isPossible = false;
-                    break;
-                }
-            }
+		// 불가능한 경우
+		// 1. 승 무 패 가 5 초과
+		// 2. 승 무 패 의 합이 5 가 아닌 경우
+		// 3. 팀의 승 무 패가 맞지 않는 경우
 
-            if (isPossible) {
-                Arrays.sort(arr, (o1, o2) -> o2[0] - o1[0]); // 승수대로 내림차순 정렬
-                // 팀 매칭
-                int idx = 0;
+		// 입력
+		for (int t = 0; t < 4; t++) {
+			String input = br.readLine();
+			StringTokenizer st = new StringTokenizer(input);
+			arr = new int[6][3];
+			ans = 1;
+			L: for (int i = 0; i < 6; i++) {
+				int sum = 0;
+				for (int j = 0; j < 3; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+					sum += arr[i][j];
+					// 1. 승 무 패 가 5 초과
+					if (arr[i][j] > 5) {
+						ans = 0;
+						break L;
+					}
+				}
+				// 2. 승 무 패 의 합이 5 가 아닌 경우
+				if (sum > 5) {
+					ans = 0;
+					break L;
+				}
+			}
 
-                match = new int[15][2];
-                for (int i = 0; i < 5; i++) {
-                    for (int j = i + 1; j < 6; j++) {
-                        match[idx][0] = i;
-                        match[idx][1] = j;
-                        idx++;
-                    }
-                }
+			visited = new boolean[6];
+			if (ans == 1) {
+				Arrays.sort(arr, (o1, o2) -> o2[0] - o1[0]);
+				recursive(0, 0);
+			}
 
-                flag = false;
-                recursive(0);
-            }
-            if(isPossible) {
-                if(flag) {
-                    ans = 1;
-                }
-                else {
-                    ans = 0;
-                }
-            } else {
-                ans = 0;
-            }
-            sb.append(ans).append(" ");
-        }
-        System.out.print(sb);
-    }
+			sb.append(ans);
+		}
+		System.out.print(sb);
+	}
 
-    private void recursive(int matchCount) {
-        // basis part
-        if (flag) {
-            return;
-        }
-
-        if (matchCount == 15) {
-            flag = true;
-            return;
-        }
-
-        // inductive part
-        int i = match[matchCount][0];
-        int j = match[matchCount][1];
-
-//        System.out.println(i + " " + j);
-
-        // 우리팀 승 상대팀 패
-        if (arr[i][0] > 0 && arr[j][2] > 0) {
-            arr[i][0]--;
-            arr[j][2]--;
-            recursive(++matchCount);
-            arr[i][0]++;
-            arr[j][2]++;
-        }
-
-        if (flag) {
-            return;
-        }
-
-        // 우리팀 무 상대팀 무
-        if (arr[i][1] > 0 && arr[j][1] > 0) {
-            arr[i][1]--;
-            arr[j][1]--;
-            recursive(++matchCount);
-            arr[i][1]++;
-            arr[j][1]++;
-        }
-
-        if (flag) {
-            return;
-        }
-
-        // 우리팀 패 상대팀 승
-        if (arr[i][2] > 0 && arr[j][0] > 0) {
-            arr[i][2]--;
-            arr[j][0]--;
-            recursive(++matchCount);
-            arr[i][2]++;
-            arr[j][0]++;
-        }
-    }
+	private void recursive(int i, int c) {
+		// basis part
+		System.out.println(Arrays.toString(visited));
+		if(i == 6)
+			return;
+		
+		// inductive part
+		if (!visited[i]) {
+			visited[i] = true;
+			if (arr[i][0] != 0 && arr[i + 1][2] != 0) {
+				arr[i][0]--;
+				arr[i+1][2]--;
+				c++;
+			} else if (arr[i][1] != 0 && arr[i+1][1] != 0) {
+				arr[i][1]--;
+				arr[i+1][1]--;
+			}
+		}
+		recursive(i + 1, c);
+		visited[i] = false;
+		recursive(i + 1, c);
+	}
 }
