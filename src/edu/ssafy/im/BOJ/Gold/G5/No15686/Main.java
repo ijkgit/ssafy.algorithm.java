a305c014326dd4e6873d377fa255b258ca295914
JAY\xenon <dlaworud@koreatech.ac.kr>
2024-02-06 00:27:41 +0900

    tmp

---
+++ src/edu/ssafy/im/BOJ/Gold/G5/No15686/Main.java
@@ -0,0 +1,20 @@
+package edu.ssafy.im.BOJ.Gold.G5.No15686;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().io();
+    }
+
+    private void io() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+
+    }
+}
