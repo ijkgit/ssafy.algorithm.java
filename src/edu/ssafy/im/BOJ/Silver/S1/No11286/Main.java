63257f5482e5e040499e396dd18b662bbace5773
JAY\xenon <dlaworud@koreatech.ac.kr>
2024-02-07 19:47:04 +0900

    BOJ 11286. 절댓값 힙

--- src/edu/ssafy/im/BOJ/Silver/S1/No11286/Main.java
+++ src/edu/ssafy/im/BOJ/Silver/S1/No11286/Main.java
@@ -5,34 +5,43 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Iterator;
 import java.util.PriorityQueue;
 
 public class Main {
-	int n;
-	
-	
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		new Main().io();
-	}
+    int n;
 
-	private void io() throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
-		n = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> pq = new PriorityQueue<>(
-				(a, b) -> {
-					int diff = Math.abs(a) - Math.abs(b);
-					if(diff == 0) return a-b;
-					else return diff;
-			});
-		
-		for (int i = 0; i < n; i++) {
-			int x = Integer.parseInt(br.readLine());
-			
-		}
-	}
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        new Main().io();
+    }
+
+    private void io() throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+                (a, b) -> {
+                    int diff = Math.abs(a) - Math.abs(b);
+                    if (diff == 0) return a - b;
+                    else return diff;
+                });
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(x);
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
