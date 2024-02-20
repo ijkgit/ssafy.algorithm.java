ce2574a531dc651bbb6eea7776f9d83aba35085f
jay <dlaworud@koreatech.ac.kr>
2024-02-20 14:55:46 +0900

    SWEA 5653. 줄기세포배양

--- src/edu/ssafy/im/SWEA/D0/No5653/Solution.java
+++ src/edu/ssafy/im/SWEA/D0/No5653/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
     private ArrayList<Point> cellList;
 	private int SIZE;
     private static final int MINVALUE = -99999;
-
+    
     class Point implements Comparable<Point> {
         int x, y, t, v;
 
@@ -20,7 +20,12 @@ public class Solution {
             this.t = t;
             this.v = t;
         }
-
+        
+        /*
+         * v는 생명력 수치, t는 시간
+         * 시간을 기준으로 내림차순 정렬 수행
+         * 만약 동시에 세포가 활성화 됐을 경우, 생명력 수치를 기준으로 내림차순 정렬 수행
+         */
         @Override
         public int compareTo(Point o) {
             if (o.t == this.t) {
@@ -45,7 +50,7 @@ public class Solution {
             m = Integer.parseInt(st.nextToken());
             k = Integer.parseInt(st.nextToken());
 
-            SIZE = Math.max(n, m) + 2 * k;
+            SIZE = Math.max(n, m) + 2 * k; // 배열의 사이즈 동적할당
             cellList = new ArrayList<>();
             visited = new boolean[SIZE][SIZE];
             for (int i = (SIZE - n) / 2; i < (SIZE + n) /2; i++) {
@@ -68,15 +73,26 @@ public class Solution {
 
     private int sol() {
         for (int now = 1; now <= k; now++) {
-            Collections.sort(cellList);
+            Collections.sort(cellList); // 내림차순 정렬 수행
             ArrayList<Point> delList = new ArrayList<>();
             for (int i = 0; i < cellList.size(); i++) {
-            	if (cellList.get(i).t == MINVALUE) break;
+            	// MINVALUE 는 비활성화된 세포, 내림차순 정렬 시 첫 발견된 비활성화세포 이후는 탐색 필요 X
+            	if (cellList.get(i).t == MINVALUE) break; 
+            	
+            	// 활성화를 위해 시간을 줄여나감
             	cellList.get(i).t--;
+            	
+            	// 활성화 조건 : 시간이 -1이 됐을 경우
+            	// 활성화를 위해 탐색 리스트에 할당 및 방문 처리
+            	// 내림차순 정렬을 수행 시에, 만약 동시에 활성화 조건을 달성했을 경우
+            	// 생명력 수치가 높은 세포를 먼저 방문하게 되며 탐색 리스트에 들어감
+            	// 따라서, 동시에 활성화 됐을 때 겹치는 영역은 생명력 수치가 높은 세포가 차지하게 됨
             	if(cellList.get(i).t == -1) {
                     visited[cellList.get(i).x][cellList.get(i).y] = true;
                     delList.add(new Point(cellList.get(i).x, cellList.get(i).y, cellList.get(i).v));
                 }
+            	
+            	// 비활성화 조건 : 시간이 -생명력 수치가 됐을 경우
             	if (cellList.get(i).t == -cellList.get(i).v) {
                 	cellList.get(i).t = MINVALUE;
                 }
@@ -88,8 +104,8 @@ public class Solution {
 
                     if(visited[nx][ny]) continue;
                     
-                    visited[nx][ny] = true;
-                    cellList.add(new Point(nx, ny, p.v));
+                    visited[nx][ny] = true; // 생명력 수치가 높은 세포가 먼저 탐색을 수행하고 먼저 번식해놓음 
+                    cellList.add(new Point(nx, ny, p.v)); 
                 }
             }
         }
