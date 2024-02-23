6ba13e78e2482b65396bb83745b5e0cab88790b6
jay <dlaworud@koreatech.ac.kr>
2024-02-23 15:41:04 +0900

    SWEA 1251. 하나로

--- src/edu/ssafy/im/BOJ/Gold/G4/No17471/Main.java
+++ src/edu/ssafy/im/BOJ/Gold/G4/No17471/Main.java
@@ -5,147 +5,127 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private boolean checkNull;
+	ArrayList<ArrayList<Integer>> graph;
 
-    public static void main(String[] args) throws IOException {
-        new Main().io();
-    }
+	public static void main(String[] args) throws IOException {
+		new Main().io();
+	}
 
-    private int n;
-    private int[] arr;
-    private int[][] graph;
-    private int[] sel;
-    private int ans = Integer.MAX_VALUE;
-    private static int TOTAL = 0;
+	private int n;
+	private int[] arr;
+	private int ans = Integer.MAX_VALUE;
 
-    private void io() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        n = Integer.parseInt(br.readLine());
+	private void io() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		n = Integer.parseInt(br.readLine());
 
-        arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            TOTAL += arr[i];
-        }
+		arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
 
-        graph = new int[n][n]; // 중복 입력 처리를 위한 인접 행렬 생성
-        checkNull = true;
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-//			int from = Integer.parseInt(st.nextToken()) - 1; // zero base
-            Integer.parseInt(st.nextToken());
-            int from = i;
-            while (st.hasMoreTokens()) {
-                checkNull = false;
-                int to = Integer.parseInt(st.nextToken()) - 1;
-                if (to == from) continue;
-                graph[from][to] = 1;
-                graph[to][from] = 1;
-            }
-        }
+		graph = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			graph.add(new ArrayList<>());
+		}
 
-//        for (int i = 0; i < arr.length; i++) {
-//            System.out.println(Arrays.toString(graph[i]));
-//        }
+		for (int from = 0; from < n; from++) {
+			st = new StringTokenizer(br.readLine());
+			int e = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < e; j++) {
+				int to = Integer.parseInt(st.nextToken()) - 1; // zero base
+				graph.get(from).add(to);
+				graph.get(to).add(from);
+			}
+		}
 
-        sol();
-        sb.append(ans);
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-    }
+		sol();
+		sb.append(ans);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 
-    private void sol() {
-        if (!checkNull) {
-            for (int i = 1; i < n; i++) {
-                sel = new int[i];
-                permutation(0, 0);
-            }
-        }
+	private void sol() {
+		for (int i = 1; i < n; i++) {
+			permutation(0, 0, new int[i]);
+		}
+	}
 
-        ans = ans == Integer.MAX_VALUE ? -1 : ans;
-    }
+	private void permutation(int k, int v, int[] sel) {
+		if (k == sel.length) {
+			System.out.println("sel : " + Arrays.toString(sel));
+			int[] rev = rev(sel);
+			if (cal(sel, rev) > ans)
+				return;
+			if (dfs(0, 1 << sel[0], sel) && dfs(0, 1 << rev[0], rev)) {
+				ans = Math.min(ans, cal(sel, rev));
 
-    private void permutation(int k, int v) {
-        if (k == sel.length) {
-//			System.out.println(Arrays.toString(sel));
-            dfs(sel[0], 1 << sel[0], 1);
-            return;
-        }
+				System.out.println("ans : " + ans);
+			}
+			return;
+		}
 
-        for (int i = 0; i < n; i++) {
-            if ((v & (1 << i)) == 0) {
-                sel[k] = i;
-                permutation(k + 1, v |= 1 << i);
-            }
-        }
-    }
+		for (int i = 0; i < n; i++) {
+			if ((v & (1 << i)) == 0) {
+				sel[k] = i;
+				permutation(k + 1, v |= 1 << i, sel);
+			}
+		}
+	}
 
-    private void setTOTAL() {
-        TOTAL = 0;
-        for (int i = 0; i < n; i++) {
-            TOTAL += arr[i];
-        }
-        for (int i = 0; i < sel.length; i++) {
-            TOTAL -= arr[sel[i]];
-        }
-    }
+	private int[] rev(int[] sel) {
+		int rev[] = new int[n - sel.length];
+		int idx = 0;
+		for (int i = 0; i < n; i++) {
+			boolean flag = false;
+			for (int j = 0; j < sel.length; j++) {
+				if (i == sel[j])
+					flag = true;
+			}
+			if (!flag) {
+				rev[idx] = i;
+				idx++;
+			}
+		}
 
-    private boolean check() {
-        int[] rev = new int[n-sel.length];
-        int idx = 0;
-        for (int i = 0; i < n; i++) {
-            boolean flag = true;
-            for (int s : sel) {
-                if (i == s) flag = false;
-            }
-            if(flag) {
-                rev[idx] = i;
-                i++;
-            }
-        }
-        return tmp(rev[0], 1 << rev[0], 1);
-    }
+		return rev;
+	}
 
-    private boolean tmp(int i, int v, int depth) {
-        if (depth == sel.length) {
-            return true;
-        }
-        for (int j = 0; j < sel.length; j++) {
-            if (graph[i][sel[j]] != 0) {
-                if ((v & (1 << sel[j])) == 0) {
-                    tmp(sel[j], v |= 1 << sel[j], depth + 1);
-                }
-            }
-        }
-        return false;
-    }
+	private boolean dfs(int k, int v, int[] sel) {
+		if (k == sel.length - 1) {
+			System.out.println("dfs : " + Arrays.toString(sel));
+			return true;
+		}
 
-    private void dfs(int i, int v, int depth) {
-        if (depth == sel.length) {
-            if (!check()) return;
-            setTOTAL();
-            int sum = 0;
-            for (int j = 0; j < sel.length; j++) {
-                sum += arr[sel[j]];
-            }
-            ans = Math.min(ans, Math.abs(TOTAL - sum));
-            return;
-        }
+		for (int s = 0; s < sel.length; s++) {
+		for (int j = 0; j < graph.get(sel[k]).size(); j++) {
+			
+				if (sel[s] == graph.get(sel[k]).get(j) && (v & (1 << graph.get(sel[k]).get(j))) == 0) {
+					return dfs(k + 1, v | 1 << graph.get(sel[k]).get(j), sel);
+				}
+			
+		}
+		}
 
-        for (int j = 0; j < sel.length; j++) {
-            if (graph[i][sel[j]] != 0) {
-                if ((v & (1 << sel[j])) == 0) {
-                    dfs(sel[j], v |= 1 << sel[j], depth + 1);
-                }
-            }
-        }
-    }
+		return false;
+	}
+
+	private int cal(int sel[], int rev[]) {
+		int s1 = 0, s2 = 0;
+		for (int s : sel)
+			s1 += arr[s];
+		for (int r : rev)
+			s2 += arr[r];
+
+		return Math.abs(s1 - s2);
+	}
 }
