c42baf4ea2e0d047f2156346c5c06f6e7b2e4b57
JAY\xenon <dlaworud@koreatech.ac.kr>
2024-02-27 00:48:22 +0900

    BOJ 2457. 공주님의 정원

--- src/edu/ssafy/im/BOJ/Gold/G3/No2457/Main.java
+++ src/edu/ssafy/im/BOJ/Gold/G3/No2457/Main.java
@@ -5,21 +5,17 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+	private ArrayList<Flower> flowerList;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		new Main().io();
 	}
 
 	private int N;
-	private int[] date;
-	private int[] monthToDay;
 	private int ans = 0;
-	private PriorityQueue<Period> periodList;
 
 	private void io() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,9 +24,7 @@ public class Main {
 
 		N = Integer.parseInt(br.readLine());
 
-		date = new int[275];
-		monthToDay = new int[] { 30, 31, 30, 31, 31, 30, 31, 30 };
-		periodList = new PriorityQueue<>((o1, o2) -> o1.si - o2.si);
+		flowerList = new ArrayList<>();
 		StringTokenizer st;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -38,98 +32,63 @@ public class Main {
 			int sd = Integer.parseInt(st.nextToken());
 			int em = Integer.parseInt(st.nextToken());
 			int ed = Integer.parseInt(st.nextToken());
-			if (sm < 3 && em < 3)
-				continue;
-			if (sm < 3 && em >= 3) {
-				sm = 3;
-				sd = 1;
-			}
-			if (sm == 12 && em == 12)
-				continue;
-			if (em == 12 && sm < 12) {
-				em = 11;
-				ed = 31;
-			}
 
-			count(sm, sd, em, ed);
+			flowerList.add(new Flower(sm * 100 + sd, em * 100 + ed));
 		}
-		check();
-		sb.append(sol());
+		sol();
+		sb.append(ans);
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 	}
 
-	private void count(int sm, int sd, int em, int ed) {
-		int si = -1, ei = -1;
+	private void sol() {
+		Collections.sort(flowerList);
 
-		for (int i = 0; i < sm - 3; i++) {
-			si += monthToDay[i];
-		}
-		si += sd;
+		int start = 301;
+		int end = 1201;
+		int now = 0;
+		int idx = 0;
 
-		for (int i = 0; i < em - 3; i++) {
-			ei += monthToDay[i];
-		}
-		ei += ed;
-
-		
-		for (int i = si; i <= ei; i++) {
-			date[i]++;
-		}
-		
-		periodList.offer(new Period(si, ei, ei - si));
-		ans++;
-	}
-
-	private void check() {
-		while (!periodList.isEmpty()) {
-			Period p = periodList.poll();
-//			System.out.println(p);
-//			System.out.println(Arrays.toString(date));
+		while(start < end) {
 			boolean flag = false;
-			for (int i = p.si; i <= p.ei; i++) {
-//				System.out.print(date[i]);
-				if (date[i] == 1) {
+
+			for (int i = idx; i < N; i++) {
+				if (flowerList.get(i).s > start) break;
+				if (flowerList.get(i).e > now) {
 					flag = true;
-					break;
+					now = flowerList.get(i).e;
+					idx = i + 1;
 				}
 			}
-//			System.out.println();
 
-			if (!flag) {
-				ans--;
-				for (int i = p.si; i <= p.ei; i++) {
-					date[i]--;
-				}
-			}
-		}
-	}
-
-	private int sol() {
-		for (int i = 0; i < date.length; i++) {
-			if (date[i] == 0) {
-				ans = 0;
+			if(flag) {
+				start = now;
+				ans++;
+			} else {
 				break;
 			}
 		}
 
-		return ans;
+		if(now < end) ans = 0;
 	}
 
-	class Period {
-		int si, ei, p;
+	class Flower implements Comparable<Flower> {
+		int s, e;
 
-		public Period(int si, int ei, int p) {
-			super();
-			this.si = si;
-			this.ei = ei;
-			this.p = p;
+		public Flower(int s, int e) {
+			this.s = s;
+			this.e = e;
 		}
 
 		@Override
-		public String toString() {
-			return "Period [si=" + si + ", ei=" + ei + ", p=" + p + "]";
+		public int compareTo(Flower o) {
+			if(this.s < o.s) return -1;
+			else if(this.s == o.s) {
+				if(this.e > o.e) return -1;
+				else if(this.e == o.e) return 0;
+				else return 1;
+			} else return 1;
 		}
 	}
 }
