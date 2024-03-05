c46c8496ae6cd6c4f7328fe2244713d0d994536e
jay <dlaworud@koreatech.ac.kr>
2024-03-05 17:56:49 +0900

    d

--- src/edu/ssafy/im/BOJ/Silver/S1/No20529/Main.java
+++ src/edu/ssafy/im/BOJ/Silver/S1/No20529/Main.java
@@ -2,15 +2,30 @@ package edu.ssafy.im.BOJ.Silver.S1.No20529;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
+	private static int T;
+	private static int N;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
+		
+		T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			N = Integer.parseInt(br.readLine());
+			
+			sol();
+		}
+	}
+
+	private static void sol() {
+		
 	}
 }
