e8b9cd6a4686a8ccdbb54dbc1a05782f0c761950
jay <dlaworud@koreatech.ac.kr>
2024-03-05 16:58:48 +0900

    BOJ 17626. Four Squares

--- src/edu/ssafy/im/BOJ/Silver/S3/No17626/Main.java
+++ src/edu/ssafy/im/BOJ/Silver/S3/No17626/Main.java
@@ -8,16 +8,31 @@ import java.io.OutputStreamWriter;
 
 public class Main {
 	private static int N;
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
-		N = Integer.parseInt(br.readLine());
-		sol(1, 0);
-	}
-
-	private static void sol(int n, int cnt) {
 		
+		N = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[N + 1];
+		dp[1] = 1;
+		
+		int min = 0;
+		for (int i = 1; i <= N; i++) {
+			min = Integer.MAX_VALUE;
+			
+			for (int j = 1; j * j <= i; j++) {
+				int k = i - j * j;
+				min = Math.min(min, dp[k]);
+			}
+			
+			dp[i] = min + 1;
+		}
+		
+		sb.append(dp[N]);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 	}
 }
