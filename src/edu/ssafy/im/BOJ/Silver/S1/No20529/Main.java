d0edbb0fb0b0009adde79b1a030d194c75432fd2
jay <dlaworud@koreatech.ac.kr>
2024-03-05 17:56:27 +0900

    20529 tmp

---
+++ src/edu/ssafy/im/BOJ/Silver/S1/No20529/Main.java
@@ -0,0 +1,16 @@
+package edu.ssafy.im.BOJ.Silver.S1.No20529;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+	}
+}
