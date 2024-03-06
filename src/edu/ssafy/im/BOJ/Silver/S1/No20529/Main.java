1042401281be365b2922b97569b1a75291b63fae
jay <dlaworud@koreatech.ac.kr>
2024-03-06 10:42:53 +0900

    BOJ 20529. 가장 가까운 세 사람의 심리적 거리

--- src/edu/ssafy/im/BOJ/Silver/S1/No20529/Main.java
+++ src/edu/ssafy/im/BOJ/Silver/S1/No20529/Main.java
@@ -16,16 +16,44 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
+
 		T = Integer.parseInt(br.readLine());
 		for (int t = 0; t < T; t++) {
+			// 입력 받기
 			N = Integer.parseInt(br.readLine());
-			
-			sol();
-		}
-	}
+			st = new StringTokenizer(br.readLine());
 
-	private static void sol() {
-		
+			// 가지치기 : 비둘기집 원리
+			// 33명 이상일 경우 무조건 세명이 겹칠 수 밖에 없다.
+			if (N > 32) {
+				sb.append(0).append("\n");
+				continue;
+			}
+
+			String[] mbti = new String[N];
+			for (int i = 0; i < N; i++) {
+				mbti[i] = st.nextToken();
+			}
+
+			int ans = Integer.MAX_VALUE;
+			L:for (int i = 0; i < N - 2; i++) {
+				for (int j = i + 1; j < N - 1; j++) {
+					for (int k = j + 1; k < N; k++) {
+						int sum = 0;
+						for (int l = 0; l < 4; l++) {
+							if (mbti[i].charAt(l) != mbti[j].charAt(l)) sum++;
+							if (mbti[j].charAt(l) != mbti[k].charAt(l)) sum++;
+							if (mbti[k].charAt(l) != mbti[i].charAt(l)) sum++;
+						}
+						ans = Math.min(ans, sum);
+						if (ans == 0) break L;
+					}
+				}
+			}
+			sb.append(ans).append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 	}
 }
