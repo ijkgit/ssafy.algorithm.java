4524f12e58bca739b348bb49ee423a15f7513b1f
jay <dlaworud@koreatech.ac.kr>
2024-03-11 15:03:19 +0900

    BOJ 1938. 통나무 옮기기

--- src/edu/ssafy/im/BOJ/Gold/G2/No1938/Main.java
+++ src/edu/ssafy/im/BOJ/Gold/G2/No1938/Main.java
@@ -1,85 +1,128 @@
 package edu.ssafy.im.BOJ.Gold.G2.No1938;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
 	private static int N;
 	private static int[][] map;
+	private static boolean[][][] v;
 	private static ArrayDeque<Point> q;
-	private static Point destination;
-	private static int[][] direction = {{0,1},{0,-1},{1,0},{-1,0}};
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	private static final int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
-		
+
 		N = Integer.parseInt(br.readLine());
 		map = new int[N][N];
-		
-		boolean f1 = false, f2 = false;
+		v = new boolean[N][N][2];
+		q = new ArrayDeque<>();
+		int cnt = 0;
 		for (int i = 0; i < N; i++) {
 			String row = br.readLine();
 			for (int j = 0; j < N; j++) {
 				map[i][j] = row.charAt(j);
-				if(!f1 && map[i][j] == 'B') {
-					if (j+1 < N && map[i][j+1] == 'B') {
-						q.offer(new Point(i, j+1, 0, 0));
+				if (map[i][j] == 'B') {
+					cnt++;
+					if (cnt != 2) continue;
+					if (1 <= j && map[i][j - 1] == 'B') {
+						v[i][j][0] = true;
+						q.offer(new Point(i, j, 0));
 					} else {
-						q.offer(new Point(i+1, j, 0, 1));
+						v[i][j][1] = true;
+						q.offer(new Point(i, j, 1));
 					}
-					f1 = true;
-				}
-				if(!f2 && map[i][j] == 'E') {
-					if (j+1 < N && map[i][j+1] == 'E') {
-						destination = new Point(i, j+1, 0);
-					} else {
-						destination = new Point(i+1, j, 1);
-					}
-					f2 = true;
 				}
 			}
 		}
-		
-		bfs();
-	}
-	// d 0 : 가로, d 1 : 세로
-	private static void bfs() {
-		q = new ArrayDeque<>();
-		boolean[][] v = new boolean[N][N];
-		v[q.peek().x][q.peek().y] = true;
-		
-		while(!q.isEmpty()) {
-			Point p = q.poll();
-			
-			for(int[] d: direction) {
-				int nx = p.x + d[0];
-				int ny = p.y + d[1];
-				
-				if (nx < 1 || ny < 1 || nx >= N-1 || ny >= N-1) continue;
-				if (v[nx])
-				v[nx][ny] = true;
-				q.offer(new Point(nx, ny, p.c+1, p.d));
-			}
-		}
-	}
-	
-	static class Point {
-		int x, y, c, d;
 
-		public Point(int x, int y, int c, int d) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.c = c;
-			this.d = d;
+		sb.append(bfs());
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+	private static int bfs() {
+		int cnt = 0;
+
+		while (!q.isEmpty()) {
+			int size = q.size();
+			for (int i = 0; i < size; i++) {
+				Point p = q.poll();
+				if (checkDestination(p.x, p.y, p.d)) return cnt;
+
+				for (int[] d : direction) {
+					int nx = p.x + d[0];
+					int ny = p.y + d[1];
+
+					if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+
+					if (checkMove(nx, ny, p.d)) {
+						q.offer(new Point(nx, ny, p.d));
+						v[nx][ny][p.d] = true;
+					}
+				}
+
+				int nd = p.d == 1 ? 0 : 1;
+				if (checkRotate(p.x, p.y, nd)) {
+					q.offer(new Point(p.x, p.y, nd));
+					v[p.x][p.y][nd] = true;
+				}
+			}
+			cnt++;
 		}
-		
+		return 0;
+	}
+
+	private static boolean checkDestination(int x, int y, int d) {
+		if (d == 1) {
+			for (int nx = x - 1; nx <= x + 1; nx++) {
+				if (nx < 0 || nx >= N) return false;
+				if (map[nx][y] != 'E') return false;
+			}
+		} else {
+			for (int ny = y - 1; ny <= y + 1; ny++) {
+				if (ny < 0 || ny >= N) return false;
+				if (map[x][ny] != 'E') return false;
+			}
+		}
+		return true;
+	}
+
+	private static boolean checkMove(int x, int y, int d) {
+		if (v[x][y][d]) return false;
+		if (d == 1) {
+			if (x == 0 || x == N - 1) return false;
+			for (int nx = x - 1; nx <= x + 1; nx++) {
+				if (map[nx][y] == '1') return false;
+			}
+		} else {
+			if (y == 0 || y == N - 1) return false;
+			for (int ny = y - 1; ny <= y + 1; ny++) {
+				if (map[x][ny] == '1') return false;
+			}
+		}
+		return true;
+	}
+
+	private static boolean checkRotate(int x, int y, int d) {
+		if (v[x][y][d]) return false;
+		for (int nx = x - 1; nx <= x + 1; nx++) {
+			for (int ny = y - 1; ny <= y + 1; ny++) {
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N) return false;
+				if (map[nx][ny] == '1') return false;
+			}
+		}
+		return true;
+	}
+
+	static class Point {
+		int x, y, d;
+
 		public Point(int x, int y, int d) {
 			this.x = x;
 			this.y = y;
