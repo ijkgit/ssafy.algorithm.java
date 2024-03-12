d6b84edbb0f86235db63a03c73d9087da9157757
jay <dlaworud@koreatech.ac.kr>
2024-03-12 10:15:42 +0900

    BOJ 9251. LCS

--- src/edu/ssafy/im/BOJ/Gold/G5/No9251/Main.java
+++ src/edu/ssafy/im/BOJ/Gold/G5/No9251/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
         a = br.readLine().toCharArray();
         b = br.readLine().toCharArray();
-        ans = new int[a.length+1][b.length+1];
+        ans = new int[a.length + 1][b.length + 1];
 
         sb.append(LCS());
         bw.write(sb.toString());
@@ -25,8 +25,8 @@ public class Main {
     private static int LCS() {
         for (int i = 1; i < ans.length; i++) {
             for (int j = 1; j < ans[0].length; j++) {
-                if (a[i-1] == b[j-1]) ans[i][j] = ans[i-1][j-1] + 1;
-                else ans[i][j] = Math.max(ans[i-1][j], ans[i][j-1]);
+                if (a[i - 1] == b[j - 1]) ans[i][j] = ans[i - 1][j - 1] + 1;
+                else ans[i][j] = Math.max(ans[i - 1][j], ans[i][j - 1]);
             }
         }
 
