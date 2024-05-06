f25fb9efb53f8c63bff676b09c689a68515d49a0
JAY\xenon <dlaworud@koreatech.ac.kr>
2024-05-07 02:42:04 +0900

    토끼와 경주 진행 중

---
+++ src/edu/ssafy/im/CodeTree/rabitAndRace/Main.java
@@ -0,0 +1,82 @@
+package edu.ssafy.im.CodeTree.rabitAndRace;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M, P;
+    private static int[][] map;
+    private static Queue<Rabbit> rabbits;
+    static class Rabbit implements Comparable<Rabbit> {
+        int pid, d, n, r, c;
+
+        public Rabbit(int pid, int d, int n, int r, int c) {
+            this.pid = pid;
+            this.d = d;
+            this.n = n;
+            this.r = r;
+            this.c = c;
+        }
+
+        @Override
+        public int compareTo(Rabbit o) {
+            if (this.n == o.n) {
+                if (this.r + this.c == o.r + o.c) {
+                    if (this.r == o.r) {
+                        if (this.c == o.c) return this.pid - o.pid;
+                        return this.c - o.c;
+                    }
+                    return this.r - o.r;
+                }
+                return (this.r + this.c) - (o.r + o.c);
+            }
+            return this.n - o.n;
+        }
+    }
+
+    private static void init(StringTokenizer st) {
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        
+        P = Integer.parseInt(st.nextToken());
+        rabbits = new PriorityQueue<>();
+        for (int p = 0; p < P; p++) {
+            int pid = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            rabbits.offer(new Rabbit(pid, d, 0, 0, 0));
+        }
+    }
+
+    private static void run(StringTokenizer st) {
+        int K = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        for (int k = 0; k < K; k++) {
+
+        }
+    }
+
+    private static void change() {
+    }
+
+    private static void finish() {
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int Q = Integer.parseInt(br.readLine());
+        for (int q = 0; q < Q; q++) {
+            st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+            if (command == 100) init(st);
+            else if (command == 200) run(st);
+            else if (command == 300) change();
+            else finish();
+        }
+    }
+}
