fb66490d999444de2f4a7298de6fcc4931ddfc56
Jay Im <dlaworud@koreatech.ac.kr>
2024-05-16 17:55:36 +0900

    BOJ 19238. 스타트 택시

--- src/edu/ssafy/im/BOJ/Gold/G2/No19238/Main.java
+++ src/edu/ssafy/im/BOJ/Gold/G2/No19238/Main.java
@@ -9,35 +9,33 @@ import java.util.StringTokenizer;
 public class Main {
     private static int N, M, E;
     private static int[][] map;
-    private static Queue<Guest> guests;
     private static Point Taxi;
     private static final int[][] direction = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    private static Point[][] targets;
 
-    static class Point {
+    static class Point implements Comparable<Point> {
         int x, y;
 
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
+
+        @Override
+        public int compareTo(Point o) {
+            if (this.x == o.x) return this.y - o.y;
+            return this.x - o.x;
+        }
     }
 
-    static class Guest implements Comparable<Guest> {
+    static class Guest {
         Point start, target;
         int cost;
 
-        public Guest(Point start, Point target) {
+        public Guest(Point start, Point target, int cost) {
             this.start = start;
             this.target = target;
-        }
-
-        @Override
-        public int compareTo(Guest g) {
-            if (this.cost == g.cost) {
-                if (this.start.x == g.start.x) return this.start.y - g.start.y;
-                return this.start.x - g.start.x;
-            }
-            return this.cost - g.cost;
+            this.cost = cost;
         }
     }
 
@@ -62,13 +60,14 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         Taxi = new Point(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1);
 
-        guests = new PriorityQueue<>();
+        targets = new Point[N][N];
         for (int m = 0; m < M; m++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken()) - 1;
             int y = Integer.parseInt(st.nextToken()) - 1;
-            guests.offer(new Guest(new Point(x, y), new Point(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1)));
-            map[x][y] = -1;
+            int rx = Integer.parseInt(st.nextToken()) - 1;
+            int ry = Integer.parseInt(st.nextToken()) - 1;
+            targets[x][y] = new Point(rx, ry);
         }
 
         sb.append(sol());
@@ -80,40 +79,41 @@ public class Main {
     private static int sol() {
         for (int m = 0; m < M; m++) {
             Guest guest = findGuest();
-            if (guest.cost == Integer.MAX_VALUE) return -1; // 모든 손님을 이동시킬 수 없는 경우
-
-            move(guest);
-            if (E < 0) return -1; // 이동하는 도중에 연료가 바닥나는 경우
+            if (guest == null) return -1; // 모든 손님을 이동시킬 수 없는 경우
+            if(!canGo(guest)) return -1; // 이동하는 도중에 연료가 바닥나는 경우
         }
         return E;
     }
 
-    private static void move(Guest guest) {
+    private static boolean canGo(Guest guest) {
         E -= guest.cost;
         Taxi.x = guest.start.x;
         Taxi.y = guest.start.y;
-        if (E <= 0) return;
+        if (E < 0) return false;
 
-        int cost = bfs(guest);
-        E -= cost;
-        if (E < 0) return;
-        else E += cost * 2;
+        int before = E;
+        if (!go(guest)) return false;
+
+        if (E >= 0) E += (before - E) * 2;
+        else return false;
 
         Taxi.x = guest.target.x;
         Taxi.y = guest.target.y;
+
+        return true;
     }
 
-    private static int bfs(Guest guest) {
-        Queue<Point> pq = new ArrayDeque<>();
-        pq.offer(new Point(Taxi.x, Taxi.y));
+    private static boolean go(Guest guest) {
+        Queue<Point> q = new ArrayDeque<>();
+        q.offer(new Point(Taxi.x, Taxi.y));
         boolean[][] visited = new boolean[N][N];
         visited[Taxi.x][Taxi.y] = true;
         int cost = 0;
-        while (!pq.isEmpty()) {
-            int size = pq.size();
+        while (!q.isEmpty()) {
+            int size = q.size();
             for (int c = 0; c < size; c++) {
-                Point p = pq.poll();
-                if (p.x == guest.target.x && p.y == guest.target.y) return cost;
+                Point p = q.poll();
+                if (p.x == guest.target.x && p.y == guest.target.y) return true;
                 for (int[] d : direction) {
                     int nx = p.x + d[0];
                     int ny = p.y + d[1];
@@ -122,34 +122,46 @@ public class Main {
                     if (visited[nx][ny] || map[nx][ny] == 1) continue;
 
                     visited[nx][ny] = true;
-                    pq.offer(new Point(nx, ny));
+                    q.offer(new Point(nx, ny));
                 }
             }
-            cost++;
+            E--;
+            if (E < 0) return false;
         }
-        return -1;
+        return false;
     }
 
     private static Guest findGuest() {
-        Queue<Point> pq = new ArrayDeque<>();
-        pq.offer(new Point(Taxi.x, Taxi.y));
+        Queue<Point> q = new ArrayDeque<>();
+        q.offer(new Point(Taxi.x, Taxi.y));
         boolean[][] visited = new boolean[N][N];
         visited[Taxi.x][Taxi.y] = true;
         int cost = 0;
-        while (!pq.isEmpty()) {
-            Point p = pq.poll();
-            if (map[p.x][p.y] == -1) return new Point(p.x);
-            for (int[] d : direction) {
-                int nx = p.x + d[0];
-                int ny = p.y + d[1];
+        Queue<Point> starts = new PriorityQueue<>();
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int c = 0; c < size; c++) {
+                Point p = q.poll();
+                if (targets[p.x][p.y] != null) starts.offer(new Point(p.x, p.y));
+                for (int[] d : direction) {
+                    int nx = p.x + d[0];
+                    int ny = p.y + d[1];
 
-                if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                if (visited[nx][ny] || map[nx][ny] == 1) continue;
+                    if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+                    if (visited[nx][ny] || map[nx][ny] == 1) continue;
 
-                visited[nx][ny] = true;
-                pq.offer(new Point(nx, ny));
+                    visited[nx][ny] = true;
+                    q.offer(new Point(nx, ny));
+                }
             }
-            return -1;
+            if (!starts.isEmpty()) {
+                Point start = starts.poll();
+                Point target = targets[start.x][start.y];
+                targets[start.x][start.y] = null;
+                return new Guest(start, target, cost);
+            }
+            cost++;
         }
+        return null;
     }
 }
